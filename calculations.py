7e7546ccfdf15f457fe1068e7664d3bd5f51029e
N.Gandhi <ng3n18@loginX001.cluster.local>
2026-08-12 17:29:54 +0100

    Cleaned up files

--- calculations.py
+++ calculations.py
@@ -12,8 +12,3 @@ def calculate_mean_and_std(energy_runs):
     mean_energy = np.mean(energy_runs)
     std_energy = np.std(energy_runs, ddof=1)
     return mean_energy, std_energy
-
-mean_energy, std_energy = calculate_mean_and_std(rgb_energy_runs)
-
-print("Mean:", mean_energy)
-print("Standard deviation:", std_energy)
